038aff44c24470159f9d63bfb6febad8fdeaeab8
rmb24 <rmb24@35.40.170.207>
2018-07-09 18:57:36 -0400

    character interface to warrior class, added cleared character to map

--- DandDGame/src/main/game/AsciiToFile.java
+++ DandDGame/src/main/game/AsciiToFile.java
@@ -12,6 +12,7 @@ public class AsciiToFile {
       out = new FileWriter("output.txt");
       
       int c;
+      int count = 1;
       int spaceCode = 0;
       int itemCode = 0;
       int monsterCode = 0;
@@ -20,37 +21,50 @@ public class AsciiToFile {
 //	  	  ',' (44) are spaces
 //    	  '!' (33) are trees
 //    	  '#' (35) are rocks
-//    	  letters correspond to a specific item
+//		  '~' (126) is water
+//    	  letters correspond to a specific item (excluding X)
 //		  numbers and :;<=>?@ chars are monsters
       
       while ((c = in.read()) != -1){
-        if(c == 44){
-          spaceCode = 1;
-        }
-        if(c == 33){
+    	  
+    	if(c == 88) { //cleared
+    		spaceCode = 1;
+    	}
+    	
+        if(c == 44){ //space
           spaceCode = 1 << 1;
         }
-        if(c == 35){
+        
+        if(c == 33){ //tree
           spaceCode = 1 << 2;
         }
-        if(c > 64 && c < 123){
+        
+        if(c == 35){ //rock
           spaceCode = 1 << 3;
+        }
+        
+        if(c > 64 && c < 123){ //item
+          spaceCode = 1 << 1;
           itemCode = c - 64; //makes item inventory go from 1-58
         }
-        if(c > 47 && c < 65 ){
-            spaceCode = 1;
+        
+        if(c > 47 && c < 65 ){ //monster
+            spaceCode = 1 << 1;
             monsterCode = c - 47;
-          }
-        if(c == 126){
-            spaceCode = 1 << 4;
-          }
+        }
+        
+        if(c == 126){ //water
+            spaceCode = 1 << 5;
+        }
+        
         if(c == 10){
           spaceCode = -1;
         }
 
         if(spaceCode != -1){
           String output = spaceCode + " " + itemCode + " " + monsterCode;
-          out.write(output + "\n");
+          out.write("space " + count + ": " + output + "\n");
+          count++;
           itemCode = 0;
           monsterCode = 0;
         }
@@ -66,4 +80,3 @@ public class AsciiToFile {
     }
   }
 }
-
