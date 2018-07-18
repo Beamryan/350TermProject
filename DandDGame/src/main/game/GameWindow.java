3a71f3ae94430edfe62962d513f33222346d0ebd
Ryan Beam <beamr@mail.gvsu.edu>
2018-07-18 19:03:20 -0400

    Better oriented gui with action listeners

--- DandDGame/src/main/game/GameWindow.java
+++ DandDGame/src/main/game/GameWindow.java
@@ -5,6 +5,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -43,36 +45,103 @@ public final class GameWindow {
 		
 		// Add action listeners
 		JButton upButton = new JButton("Up");
-		upButton.setPreferredSize(new Dimension(250, 100));
+		upButton.setPreferredSize(new Dimension(250, 90));
 		upButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		upButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move up method
+		  }
+		});
 		
 		JButton downButton = new JButton("Down");
-		downButton.setPreferredSize(new Dimension(250, 100));
+		downButton.setPreferredSize(new Dimension(250, 90));
 		downButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		downButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move down method
+		  }
+		});
 		
 		JButton leftButton = new JButton("Left");
-		leftButton.setPreferredSize(new Dimension(250, 100));
+		leftButton.setPreferredSize(new Dimension(250, 90));
 		leftButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		leftButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move left method
+		  }
+		});
 		
 		JButton rightButton = new JButton("Right");
-		rightButton.setPreferredSize(new Dimension(250, 100));
+		rightButton.setPreferredSize(new Dimension(250, 90));
 		rightButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		rightButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move right method
+		  }
+		});
 		
 		JButton inventoryButton = new JButton("Inventory");
-		inventoryButton.setPreferredSize(new Dimension(250, 100));
+		inventoryButton.setPreferredSize(new Dimension(250, 90));
 		inventoryButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		inventoryButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move inventory method
+		  }
+		});
 		
 		JButton attackButton = new JButton("Attack");
-		attackButton.setPreferredSize(new Dimension(250, 100));
+		attackButton.setPreferredSize(new Dimension(250, 90));
 		attackButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		attackButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move attack method
+		  }
+		});
 		
 		JButton shieldButton = new JButton("Shield");
-		shieldButton.setPreferredSize(new Dimension(250, 100));
+		shieldButton.setPreferredSize(new Dimension(250, 90));
 		shieldButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		shieldButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move shield method
+		  }
+		});
 		
-		JButton runButton = new JButton("Run");
-		runButton.setPreferredSize(new Dimension(250, 100));
-		runButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		JButton restButton = new JButton("Rest");
+		restButton.setPreferredSize(new Dimension(250, 90));
+		restButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		restButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move run method
+		  }
+		});
+		
+		JButton fleeButton = new JButton("Flee");
+		fleeButton.setPreferredSize(new Dimension(250, 90));
+		fleeButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		fleeButton.addActionListener(new ActionListener()
+		{
+		  public void actionPerformed(ActionEvent e)
+		  {
+		    // Call the move flee method
+		  }
+		});
 		
 		buttonPanel.add(upButton);
 		buttonPanel.add(downButton);
@@ -81,7 +150,8 @@ public final class GameWindow {
 		buttonPanel.add(inventoryButton);
 		buttonPanel.add(attackButton);
 		buttonPanel.add(shieldButton);
-		buttonPanel.add(runButton);
+		buttonPanel.add(restButton);
+		buttonPanel.add(fleeButton);
 		
 		mainFrame.add(gamePanel, BorderLayout.CENTER);
 		mainFrame.add(textPanel, BorderLayout.PAGE_END);
